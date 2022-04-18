bf5ebc5bcd0727379806fba4da173dc30a5b1021
Sathish Dayalan <sk48209@gmail.com>
2022-04-18 13:00:45 +0530

    created the complete project

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,74 @@
+import { Link, NavLink } from "react-router-dom";
+import "./index.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faHome } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faSkype,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons";
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="sathish" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sathish-dayalan/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/sathish48209"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.youtube.com/c/TraversyMedia"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="skype:live:bobangajicsm">
+            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
